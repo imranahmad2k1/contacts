dbb5ebf7447c63ff75a12d3d50a587c365946f2b
Imran Ahmad <meetimrang@gmail.com>
2023-05-15 03:32:30 +0500

    initial commit

---
+++ android/app/src/main/kotlin/com/learningflutter/contacts_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.learningflutter.contacts_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: 'Flutter Demo',
+    theme: ThemeData(
+      colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      useMaterial3: true,
+    ),
+    home: const MyHomePage(),
+  ));
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('Home Page'),
+      ),
+    );
+  }
+}
